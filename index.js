51340704a78946eb50fea30476f13bcb308f83e8
omkarvh <omkarvh56@gmail.com>
2024-07-21 09:07:08 +0530

    New graphs

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
+//graphs
+var xValues = ["Html", "Css", "Bootstrap 5", "Vanilla Js", "Jquery",""];
+var yValues = [65, 48, 75, 55, 45,0];
+var barColors = ["orange", "skyblue","blue","orange","blue"];
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Front End technologies "
+    }
+  }
+});
+
+var x2Values = ["Node js", "Express", "Git", "Postgre SQL", "My SQL",""];
+var y2Values = [55, 65, 75, 68, 45,0];
+var barColors = ["Green", "white","orange","blue","brown"];
+
+new Chart("myChart2", {
+  type: "bar",
+  data: {
+    labels: x2Values,
+    datasets: [{
+      backgroundColor: barColors,
+      data: y2Values
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Back End technologies and Data base"
+    }
+  }
+});
+
+//graphs end
+
+
+
+//contact form 
+
 function submitForm() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
   
     // Validate form fields
     if (name === "" || email === "" || phone === "") {
@@ -17,3 +68,8 @@ function submitForm() {
     phone = "";
 
   }
+
+// git hub update 
+
+
+
